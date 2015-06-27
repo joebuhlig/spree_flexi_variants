aca26b5ebd55b286f2ba98d4020f3c989bdc0dd4
Alex Girdler <me@alexgirdler.com>
2015-06-26 17:39:30 -0700

    Updates

--- app/overrides/flexi_variants_admin_tabs.rb
+++ app/overrides/flexi_variants_admin_tabs.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
+Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_product",
                      :name => "flexi_variants_admin_tabs",
-                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                     :insert_after => "erb[loud]:contains('tab :optioin_types')",
                      :text => "<%= tab(:product_customization_types, :url => spree.admin_product_customization_types_path) %>")
