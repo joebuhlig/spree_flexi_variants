2356b6f6bb8dc6d5826ffbf343ee9a51e6d2814c
Alex Girdler <me@alexgirdler.com>
2015-06-26 17:46:46 -0700

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.prepend do
+Spree::Core::Engine.add_routes do
   match 'product_customizations/price', :to => 'product_customizations#price'
 
   match 'customize/:product_id', :to => 'products#customize', :as => 'customize'
